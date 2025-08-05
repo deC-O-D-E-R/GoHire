cab204fe9ca00bd4a32bfa49d6feb80498142a42
Mohd Saif <mohdsaif9990197398@gmail.com>
2025-08-05 16:45:06 +0530

    CORS updated

--- ml-service/main.py
+++ ml-service/main.py
@@ -10,9 +10,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
